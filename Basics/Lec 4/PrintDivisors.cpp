0f18ad03a449ca43c0728f7e16c11689c89e66fc
Kartik <kartikkaushik7497@gmail.com>
2025-06-25 14:41:25 +0530

    PrintDivisor.cpp Updated Optimal soln

--- Basics/Lec 4/PrintDivisors.cpp
+++ Basics/Lec 4/PrintDivisors.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int *PrintDivisors(int N, int &size)
+int *PrintDivisors_brute(int N, int &size)
 {
     int count = 0;
     int *divisors = new int[N];
@@ -17,14 +17,30 @@ int *PrintDivisors(int N, int &size)
     return divisors;
 }
 
+void *PrintDivisors_optimal(int N)
+{
+    for (int i = 1; i <= sqrt(N); i++)
+    {
+        if (N % i == 0)
+        {
+            cout << i << " ";
+        }
+        if (N % i != i)
+        {
+            cout << N / i << " ";
+        }
+    }
+}
+
 int main()
 {
     int N = 12;
     int size;
-    int *divisors = PrintDivisors(N, size);
+    int *divisors = PrintDivisors_brute(N, size);
     for (int i = 0; i < size; i++)
     {
         cout << divisors[i] << " ";
     }
+    // PrintDivisors_optimal(N, size);
     return 0;
 }
