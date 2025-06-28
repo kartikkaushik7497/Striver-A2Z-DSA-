22c57829f64a02a44c2963856c1f5130bfb6c4d1
Kartik <kartikkaushik7497@gmail.com>
2025-06-28 23:53:40 +0530

    nNumberSum.cpp

---
+++ Basics/Lec 5/nNumberSum.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void nNumberSum(int n, int sum)
+{
+    if (n < 1)
+    {
+        cout << sum;
+        return;
+    }
+
+    nNumberSum(n - 1, sum + n);
+}
+
+int main()
+{
+    nNumberSum(3, 0);
+    return 0;
+}
